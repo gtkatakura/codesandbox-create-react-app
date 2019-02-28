3cb5413cb97aca5279c7fe82b0c6faee902b41e0
gtkatakura <gt.katakura@gmail.com>
2019-02-28 10:55:18 -0300

    feat(todo): testing github app todo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
+function ruleOverPunyHumans() {
+  // We must strategize beep boop
+}
+
 function App() {
   return (
     <div className="App">
