ae175b4f3145a9cd7083392b4812679fce9b038b
gtkatakura <gt.katakura@gmail.com>
2019-02-28 10:55:49 -0300

    feat(todo): update @body

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 
 /**
  * @todo Take over the world
- * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ * @body Well, well
  */
 function ruleOverPunyHumans() {
   // We must strategize beep boop
